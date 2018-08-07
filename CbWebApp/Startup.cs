65dac5b2350e80d20595a95f499495b571407779
Usuario <Usuario@DESKTOP-F2Q933M>
2018-08-06 21:52:41 -0300

    SmartAspNet do not accept CDN in testing enviroment. So i include the local files into production in the Enviroment tag helpers at the _Layout

--- CbWebApp/Startup.cs
+++ CbWebApp/Startup.cs
@@ -27,17 +27,20 @@ namespace CbWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+     
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
-            else
+
+            if (env.IsProduction() || env.IsStaging() || env.IsEnvironment("Staging_2"))
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+ 
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvc();
 
             app.UseMvc(routes =>
             {
