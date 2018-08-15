ea8b3a541402dec0066091e6a26ab26e152d891f
Usuario <Usuario@DESKTOP-F2Q933M>
2018-08-15 16:19:44 -0300

    atualização

--- CbWebApp/Startup.cs
+++ CbWebApp/Startup.cs
@@ -18,13 +18,11 @@ namespace CbWebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
      
